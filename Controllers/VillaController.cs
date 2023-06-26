30a2c82f576f0639623ec34d28c61612176afbe4
hahs <alexandre_2420@outlook.com>
2023-06-26 17:44:41 -0500

    create our first endpoint

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id=1, Name="Vista a la piscina"},
+                new Villa { Id=2, Name="Vista a la playa"},
+            };
+        }
+    }
+}
