9f18b0347f1fee0e3286c68382f2596c2cd1283c
hahs <alexandre_2420@outlook.com>
2023-06-27 12:28:39 -0500

    add villas for default

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,7 +5,11 @@ namespace MagicVilla_API.Data
 {
     public class ApplicationDbContext: DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            // soluciona un error con las fechas
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
        
 
         // de esta manera expeficamos que se va a crear una tabla
@@ -14,6 +18,34 @@ namespace MagicVilla_API.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql("Host=localhost;Database=MagicVilla;Username=postgres;Password=TravelMate2420");
+
+        // nos permite crear datos por default
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+               new Villa()
+               {
+                   Id = 1,
+                   Name = "villa test",
+                   Description = "Description test",
+                   ImageUrl = "https://image.com",
+                   Price = 999,
+                   Area = 60,
+                   CreatedAt = DateTime.Now,
+                   UpdatedAt = DateTime.Now
+               },
+               new Villa()
+               {
+                   Id = 2,
+                   Name = "villa test 02",
+                   Description = "Description test 02",
+                   ImageUrl = "https://image02.com",
+                   Price = 990,
+                   Area = 40,
+                   CreatedAt = DateTime.Now,
+                   UpdatedAt = DateTime.Now
+               });
+        }
 
     }
 }
---
+++ Migrations/20230627171752_RetroalimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RetroalimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Area", "CreatedAt", "Description", "ImageUrl", "Name", "Price", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, 60, new DateTime(2023, 6, 27, 12, 17, 52, 331, DateTimeKind.Local).AddTicks(995), "Description test", "https://image.com", "villa test", 999.0, new DateTime(2023, 6, 27, 12, 17, 52, 331, DateTimeKind.Local).AddTicks(1011) },
+                    { 2, 40, new DateTime(2023, 6, 27, 12, 17, 52, 331, DateTimeKind.Local).AddTicks(1013), "Description test 02", "https://image02.com", "villa test 02", 990.0, new DateTime(2023, 6, 27, 12, 17, 52, 331, DateTimeKind.Local).AddTicks(1014) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
